509cca53fcbf4dc62df2f9eca04cc40744bbe4f9
PDAWhite <96366518+PDAWhite@users.noreply.github.com>
2024-11-25 12:01:52 +0300

    Update Main.java
    for some reason, there was old main. now it's fine

--- Main.java
+++ Main.java
@@ -1,14 +1,27 @@
 import ru.ifmo.se.pokemon.Battle;
-import ru.ifmo.se.pokemon.Pokemon;
 import pokemons.Regirock;
+import pokemons.Eevee;
+import pokemons.Espeon;
+import pokemons.Budew;
+import pokemons.Roselia;
+import pokemons.Roserade;
+
 public class Main {
-	public static void main(String[] args){
-		Battle b = new Battle();
-		Regirock p1 = new Regirock("Космический камень", 1);
-		Pokemon p2 = new Pokemon("Хищник", 1);
-		b.addAlly(p1);
-		b.addFoe(p2);
-		b.go();
-	}
-	
+        public static void main(String[] args){
+                Battle b = new Battle();
+                Regirock rock = new Regirock("Космический камень", 10);
+                Eevee whatdoesthefoxsay= new Eevee("Котолис", 10);
+                Espeon isthatacatfromshoj= new Espeon("Сноед", 10);
+                Budew seedling= new Budew("Проросток", 10);
+                Roselia roses= new Roselia("Розы", 10);
+                Roserade ultimateroses= new Roserade("Розы 2.0", 10);
+                b.addAlly(rock);
+                b.addAlly(whatdoesthefoxsay);
+                b.addAlly(roses);
+                b.addFoe(ultimateroses);
+                b.addFoe(isthatacatfromshoj);
+                b.addFoe(seedling);
+                b.go();
+        }
+
 }
